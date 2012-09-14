2f9df7cb411d9240fb725a276afcfd176bf603a6
Lukas Koudela <lukass.koudela@gmail.com>
2012-09-13 20:37:02 -0700

    improvement to run on localhost

--- femtec2013/femtec/urls.py
+++ femtec2013/femtec/urls.py
@@ -1,12 +1,19 @@
 from django.conf.urls.defaults import patterns, include
 from django.contrib import admin
 
+from django.conf import settings
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
     (r'^', include('femtec.site.views')),
     (r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    )
 
 handler404 = 'femtec.site.views.handler404'
 handler500 = 'femtec.site.views.handler500'
