061b1b4716721fdd84cb9025fff2f107cd50b191
Lukas Koudela <lukass.koudela@gmail.com>
2012-12-07 17:09:41 +0100

    replacement of _ in email by _

--- femtec2013/femtec/site/latex.py
+++ femtec2013/femtec/site/latex.py
@@ -169,7 +169,7 @@ class Author(Latexible):
         self.first_name = first_name
         self.last_name = last_name
         self.address = address
-        self.email = email
+        self.email = email.replace('_', '\_')
         self.presenting = presenting
 
     def to_latex(self, single_author=False):
